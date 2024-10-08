f12fcbff927e4f3b461d9fe01815e0a81e226139
Shea Martin <shea@brewninja.net>
2024-10-08 13:47:56 -0300

    export ICompileOptions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { getNearestTsConfigCompilerOptions } from './getNearestTsConfigCompilerOptions.js';
 import { loadTsConfig } from './loadTsConfig.js';
-import { CacheConfig, CompileResult } from './types.js';
+import { CacheConfig, CompileResult, ICompileOptions } from './types.js';
 
 export { loadTsConfig, getNearestTsConfigCompilerOptions };
-export type { CacheConfig, CompileResult };
+export type { CacheConfig, CompileResult, ICompileOptions };
